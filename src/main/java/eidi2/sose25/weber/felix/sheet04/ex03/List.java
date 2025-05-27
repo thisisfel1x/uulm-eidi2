6ee9c4d0f3e828b88cf8244a0ee87e41ae7e90d3
thisisfel1x <felixweber271@gmail.com>
2025-05-27 17:26:01 +0200

    feat(sheet04): finalize assignment 3
    Took 13 minutes

--- src/main/java/eidi2/sose25/weber/felix/sheet04/ex03/List.java
+++ src/main/java/eidi2/sose25/weber/felix/sheet04/ex03/List.java
@@ -42,20 +42,96 @@ public class List<T> {
     }
 
     public void insert(T val, int index) {
+        if (index <= 0) {
+            // Verhalten wie prepend
+            this.prepend(val);
+            return;
+        }
 
+        Element<T> newElement = new Element<>(val);
+
+        Element<T> current = this.head;
+        int i = 0;
+
+        while (current != null && i < index - 1) {
+            current = current.getNext();
+            i++;
+        }
+
+        // Wenn current == null ist, wurde das Ende der Liste erreicht; wir nutzen append
+        if (current == null || current.getNext() == null) {
+            this.append(val);
+        } else {
+            // Ansonsten können wir ganz easy ein neues Element in die Liste einfügen
+            newElement.setNext(current.getNext());
+            current.setNext(newElement);
+        }
+
+        if (newElement.getNext() == null) {
+            this.tail = newElement;
+        }
     }
 
     public T get(int n) {
+        if (n < 0) throw new IndexOutOfBoundsException();
 
+        Element<T> current = this.head;
+        int i = 0;
+
+        while (current != null && i < n) {
+            current = current.getNext();
+            i++;
+        }
+
+        if (current == null) throw new IndexOutOfBoundsException();
+
+        return current.getValue();
     }
 
-    public boolean contains(T val) {
 
+    public boolean contains(T val) {
+        Element<T> current = head;
+
+        while (current != null) {
+            if ((val == null && current.getValue() == null) ||
+                    (val != null && val.equals(current.getNext()))) {
+                return true;
+            }
+            current = current.getNext();
+        }
+
+        return false;
     }
 
     public void remove(int n) {
+        if (n < 0 || this.head == null) throw new IndexOutOfBoundsException();
 
+        if (n == 0) {
+            this.head = this.head.getNext();
+            if (this.head == null) this.tail = null;
+            return;
+        }
+
+        Element<T> current = this.head;
+        int i = 0;
+
+        while (current.getNext() != null && i < n - 1) {
+            current = current.getNext();
+            i++;
+        }
+
+        if (current.getNext() == null) throw new IndexOutOfBoundsException();
+
+        // Element entfernen
+        Element<T> toRemove = current.getNext();
+        current.setNext(toRemove.getNext());
+
+        // tail anpassen, falls letztes Element entfernt wurde
+        if (current.getNext() == null) {
+            this.tail = current;
+        }
     }
+
 
     @Override
     public String toString() {
